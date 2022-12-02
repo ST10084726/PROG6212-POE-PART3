0baec52e8be1aba94831b351f12245221ff619e7
SyurenChetty <104988398+SyurenChetty@users.noreply.github.com>
2022-12-02 18:22:36 +0200

    Add files via upload

---
+++ Models/Module.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuleCalculatorWeb.Models
+{
+    public class Module
+    {
+        //Variables of the Class
+        [Key]
+        public int moduleID { get; set; }
+        public string ModuleCode { get; set; }
+        public string ModuleName { get; set; }
+        public int Credits { get; set; }
+        public int HoursPerWeek { get; set; }
+        public int SelfStudy { get; set; }
+        public int Weeks_in_Semester { get; set; }
+        public int HoursStudyFor { get; set; }
+        public System.DateTime StudyDate { get; set; }
+        public int ReaminingHoursLeft { get; set; }
+
+    }
+}
---
+++ Models/Semester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuleCalculatorWeb.Models
+{
+    public class Semester
+    {
+        //Variables of the Class
+        [Key]
+        public int SemId { get; set; }
+        public string semesterName { get; set; }
+        public System.DateTime startDate { get; set; }
+        public int Weeks { get; set; }
+    }
+}
